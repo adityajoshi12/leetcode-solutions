bd8fc738ecde847e7db73bf677bf5d69d593a93a
Aditya Prakash Joshi <adityaprakashjoshi1@gmail.com>
2021-08-14 11:47:44 +0530

    Length of Last Word

---
+++ 58. Length of Last Word.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int count=0;
+        for(int i=s.length()-1;i>=0;i--){
+            if( s.charAt(i)==' ' && count==0 ){
+
+                continue;
+            }
+			
+            if( s.charAt(i)==' ' && count!=0 ) {
+ 
+                break;
+            }
+            
+            count++;
+        }
+        return count;
+    }
+}
