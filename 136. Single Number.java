be1a42683c6d90c3e1f1837bf4825ccce6189c09
Aditya Prakash Joshi <adityaprakashjoshi1@gmail.com>
2021-11-15 09:39:45 +0530

    Update 136. Single Number.java

--- 136. Single Number.java
+++ 136. Single Number.java
@@ -11,3 +11,15 @@ class Solution {
         return -1;
    }
 }
+
+// alternate solution
+/* class Solution {
+    public int singleNumber(int[] nums) {
+       int result=0;
+        
+        for(int a : nums){
+            result ^=a;
+        }
+        return result;
+     }
+} */
